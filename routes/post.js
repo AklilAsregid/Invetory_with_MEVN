2017cd5a3fc7ce142cd387bf0cda6c96095b5bf1
Aklil Asregid <scotmiller84@gmail.com>
2021-05-07 01:42:36 -1200

    backend 0.1

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,8 @@
 const router = require('express').Router()
 const protect = require('./verifyToken')
+const mongoose = require('mongoose')
+
+
 router.get('/', protect, (req, res) => {
   res.json({
     posts: {
@@ -8,6 +11,20 @@ router.get('/', protect, (req, res) => {
     }
   })
   res.send(req.User)
+
+})
+//this routes will be used for listing all invetory item
+router.get('/invetory',protect,(req, res)=>{
+  
+})
+//this routes will be used for adding new item
+router.post('/invetory/add',protect,(req, res)=>{
+
+})
+//this routes will be used for deleting a single item
+//this routes will be used for deleteing a single item
+//using multer i will add image for each item we have
+router.delete('/invetory/:id',protect,(req, res)={
 
 })
 
